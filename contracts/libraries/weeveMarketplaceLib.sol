39c29aa66ad1dd2161bcdf4d3892012e63c6b87b
marvinkruse <marvin.kruse@icloud.com>
2018-05-08 15:18:46 +0200

    First version of the marketplaces (lib-based)

---
+++ contracts/libraries/weeveMarketplaceLib.sol
@@ -0,0 +1,130 @@
+pragma solidity ^0.4.23;
+
+import "./SafeMath.sol";
+
+// Basic ERC20 Interface 
+interface ERC20 {
+    function totalSupply() external returns (uint);
+    function balanceOf(address tokenOwner) external returns (uint balance);
+    function allowance(address tokenOwner, address spender) external returns (uint remaining);
+    function transfer(address to, uint tokens) external returns (bool success);
+    function approve(address spender, uint tokens) external returns (bool success);
+    function transferFrom(address from, address to, uint tokens) external returns (bool success);
+}
+
+library weeveMarketplace {
+    using SafeMath for uint;
+
+    struct Trade {
+        string tradeID;
+        address seller;
+        uint256 price;        
+        uint256 amount; 
+        bool paid;
+    }
+
+    struct Validator {
+        address validatorAddress;
+        uint256 stakedTokens;
+    }
+
+    struct Arbiter {
+        address arbiterAddress;
+        uint256 stakedTokens;
+    }
+    
+    struct MarketplaceStorage {
+        // DeviceID maps to Device-Struct
+        mapping (string => Trade) trades;
+
+        // Address maps to array of strings
+        mapping (address => string[]) sellsOfUser;
+
+        // Address maps to array of strings
+        mapping (address => string[]) buysOfUser;
+
+        // Address to Validator-Struct
+        mapping (address => Validator) validators;
+
+        // Address to Arbiter-Struct
+        mapping (address => Arbiter) arbiters;
+
+        // Number of all active trades
+        uint256 currentTrades;
+
+        // Current activation state of the marketplace itself
+        bool marketplaceIsActive;
+
+        // Commission of each trade that will be deducted in percent
+        uint256 commission;
+
+        // Currently acrued commission of this marketplace
+        uint256 commissionBalance;
+
+        // Access to our token
+        ERC20 token;
+    }
+
+    // Posting a new trade offer to the marketplace by ID, price and amount
+    function sell(MarketplaceStorage storage myMarketplaceStorage, string _tradeID, uint256 _price, uint256 _amount) public returns (bool) {
+        // Price and amount both need to be larger than zero
+        require(_price > 0 && _amount > 0);
+
+         // Allocate new trade
+        weeveMarketplace.Trade memory newTrade;
+
+        // Setting basic values of this trade
+        newTrade.tradeID = _tradeID;
+        newTrade.seller = msg.sender;
+        newTrade.price = _price;
+        newTrade.amount = _amount;
+
+        // Adding this trade to the general trade mapping
+        myMarketplaceStorage.trades[_tradeID] = newTrade;
+
+        // Pushing this trade into the users sell-array
+        myMarketplaceStorage.sellsOfUser[msg.sender].push(_tradeID);
+
+        // Increasing the number of currently active trades by one
+        myMarketplaceStorage.currentTrades = myMarketplaceStorage.currentTrades.add(1);
+
+        return true;
+    }
+
+    // Accepting a trade offer from the marketplace by ID
+    function buy(MarketplaceStorage storage myMarketplaceStorage, string _tradeID) public returns (bool) {
+        // Sending the price of the trade to the markteplace
+        require(myMarketplaceStorage.token.transferFrom(msg.sender, address(this), myMarketplaceStorage.trades[_tradeID].price));
+
+        // Paying the seller of this trade (price minus commission)
+        require(paySeller(myMarketplaceStorage, myMarketplaceStorage.trades[_tradeID].seller, myMarketplaceStorage.trades[_tradeID].price));
+
+        // Adding this trade to the buys of the user
+        myMarketplaceStorage.buysOfUser[msg.sender].push(_tradeID);
+
+        // Marking this trade as paid
+        myMarketplaceStorage.trades[_tradeID].paid = true;
+
+        // Decreasing the number of currently active trades by oen
+        myMarketplaceStorage.currentTrades = myMarketplaceStorage.currentTrades.sub(0);
+
+        return true;
+    }
+
+    // Internal: paying the seller of a trade (with deduction of the commission)
+    function paySeller(MarketplaceStorage storage myMarketplaceStorage, address _sellerAddress, uint256 _amountOfTokens) internal returns (bool) {
+        // Calculating the commission on tokens
+        uint256 deductedCommission = _amountOfTokens.mul(myMarketplaceStorage.commission).div(100);
+
+        // Calculating the amount that will be paid to the seller
+        uint256 payOut = _amountOfTokens.sub(deductedCommission);
+
+        // Transfering the pay-out tokens to the seller
+        require(myMarketplaceStorage.token.transfer(_sellerAddress, payOut));
+
+        // Adding the commission to the commission-balance of this marketplace
+        myMarketplaceStorage.commissionBalance = myMarketplaceStorage.commissionBalance.add(deductedCommission);
+
+        return true;
+    }
+}
---
+++ contracts/weeveMarketplace.sol
@@ -0,0 +1,148 @@
+pragma solidity ^0.4.23;
+
+import "./libraries/weeveMarketplaceLib.sol";
+import "./libraries/Owned.sol";
+
+// An exemplary weeve marketplace contract
+contract myMarketplace is Owned {    
+    using SafeMath for uint;
+
+    // General storage for the marketplace
+    weeveMarketplace.MarketplaceStorage public myMarketplaceStorage;
+
+    // Address of the weeve Factory
+    address public weeveFactoryAddress = 0x0000000000000000000000000000000000000000;
+
+    // Address of the WEEV token
+    address public weeveTokenAddress = 0x0000000000000000000000000000000000000000;
+
+    // Name of the marketplace
+    string public marketplaceName;
+
+    // Constructor (fired once upon creation)
+    constructor() public {
+        // Initially the marketplace is not active
+        myMarketplaceStorage.marketplaceIsActive = false;
+    }
+
+    function initialize(string _name, uint256 _commission, address _owner) public returns(bool){
+        // Only the weeve factory is able to initialise a marketplace
+        require(msg.sender == weeveFactoryAddress);
+
+        // Setting the name of the marketplace
+        marketplaceName = _name;
+
+        // Setting the owner of the marketplace
+        require(_owner != address(0));
+        owner = _owner;
+
+        // Number of all active devices
+        myMarketplaceStorage.currentTrades = 0;
+
+        // Activation of the marketplace
+        myMarketplaceStorage.marketplaceIsActive = true;
+
+        // Setting a commission percentage for each trade
+        require(_commission >= 0 && _commission < 100);
+        myMarketplaceStorage.commission = _commission;
+
+        // Initializing the the balance of the currently collected commission
+        myMarketplaceStorage.commissionBalance = 0;
+
+        // Activation of the marketplace
+        myMarketplaceStorage.marketplaceIsActive = true;
+
+        // Setting the address of the WEEV erc20 token
+        myMarketplaceStorage.token = ERC20(weeveTokenAddress);
+        
+        return true;
+    }
+
+    // Posting a new trade offer to the marketplace by ID, price and amount
+    function sell(string _tradeID, uint256 _price, uint256 _amount) public marketplaceIsActive {
+        require(weeveMarketplace.sell(myMarketplaceStorage, _tradeID, _price, _amount));
+    }
+
+    // Accepting a trade offer from the marketplace by ID
+    function buy(string _tradeID) public marketplaceIsActive {
+        require(weeveMarketplace.buy(myMarketplaceStorage, _tradeID));
+    }
+
+    // Withdrawing the acrued commission from the marketplace as the owner
+    function withdrawCommission(address _recipientAddress, uint256 _amountOfTokens) public marketplaceIsActive onlyOwner {
+        // Number of tokens that will be withdrawn must be smaller or equal to the balance of the commission
+        require(_amountOfTokens <= myMarketplaceStorage.commissionBalance);
+
+        // Transfering the tokens to the recipient
+        require(myMarketplaceStorage.token.transfer(_recipientAddress, _amountOfTokens));
+
+        // Updating the commission balance
+        myMarketplaceStorage.commissionBalance = myMarketplaceStorage.commissionBalance.sub(_amountOfTokens);
+    }
+
+    // Returns the trade information
+    function getTrade(string _tradeID) public view returns(string tradeID, address seller, uint256 price, uint256 amount, bool paid) {
+        return(myMarketplaceStorage.trades[_tradeID].tradeID, myMarketplaceStorage.trades[_tradeID].seller, myMarketplaceStorage.trades[_tradeID].price, myMarketplaceStorage.trades[_tradeID].amount, myMarketplaceStorage.trades[_tradeID].paid);
+    }
+
+    // Returns amount of devices that an address has in this marketplace
+    function getTotalTradeCount() public view returns (uint256 numberOfCurrentTrades) {
+        return myMarketplaceStorage.currentTrades;
+    }
+
+    // Changeing the commission of this marketplace as the owner
+    function changeCommission(uint256 _commission) public marketplaceIsActive onlyOwner {
+        require(_commission >= 0 && _commission < 100);
+        myMarketplaceStorage.commission = _commission;
+    }
+
+    function addValidator(address _address) public marketplaceIsActive onlyOwner {
+        myMarketplaceStorage.validators[_address].validatorAddress = _address;
+    }
+
+    function removeValidator(address _address) public marketplaceIsActive onlyOwner {
+        delete myMarketplaceStorage.validators[_address];
+    }
+
+    function addArbiter(address _address) public marketplaceIsActive onlyOwner {
+        myMarketplaceStorage.arbiters[_address].arbiterAddress = _address;
+    }
+
+    function removeArbiter(address _address) public marketplaceIsActive onlyOwner {
+        delete myMarketplaceStorage.arbiters[_address];
+    }
+    
+    function checkValidatorStatus(address _address) public view returns (bool status) {
+        if(myMarketplaceStorage.validators[_address].validatorAddress == _address) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    function checkArbiterStatus(address _address) public view returns (bool status) {
+        if(myMarketplaceStorage.arbiters[_address].arbiterAddress == _address) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    // Modifier: Checks whether an address is a validator
+    modifier isValidator(address _address) {
+        require(myMarketplaceStorage.validators[_address].validatorAddress == _address);
+        _;
+    }
+
+    // Modifier: Checks whether an address is an arbiter
+    modifier isArbiter(address _address) {
+        require(myMarketplaceStorage.arbiters[_address].arbiterAddress == _address);
+        _;
+    }
+
+    // Modifier: Checks whether this marketplace is activated
+    modifier marketplaceIsActive() {
+        require(myMarketplaceStorage.marketplaceIsActive);
+        _;
+    }
+}
